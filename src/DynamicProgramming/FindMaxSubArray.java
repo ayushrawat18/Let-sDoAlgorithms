a17f2057c841f7d71b4fc8182fc9331d1e0df843
ayush.rawat <ayush.rawat@servicenow.com>
2021-09-24 18:18:16 +0530

    Maximum sum problem using Divide and Conquer

---
+++ src/DynamicProgramming/FindMaxSubArray.java
@@ -0,0 +1,61 @@
+package DynamicProgramming;
+
+/**
+ * Solution will find the maximum subarray using Divide and Conquer technique
+ */
+public class FindMaxSubArray {
+
+    public static void main(String args[]) {
+
+        // In this array we need to find an subarray where sum is maximum
+        //int arr[] = new int[] {13, -2 , -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7 };
+        int arr[] = new int[] { -2, -7 ,-1, -4, -2 , -6};
+       int sum =  _findMaxSubArray( 0, arr.length - 1, arr);
+       System.out.println("Max sum is "+sum);
+
+    }
+
+    public static int _findMaxSubArray(int low, int high, int array[]) {
+        if (low == high )
+            return (array[low]);
+        else {
+            int mid = (low + high)/2;
+            int lss = _findMaxSubArray(low, mid, array);
+            int rss = _findMaxSubArray(mid+1, high, array);
+            int css = _findMaxCrossingArraySum(low, mid, high, array);
+
+            if (lss >= rss && lss >=css)
+                return lss;
+            else if (rss >= lss && rss >= css)
+                return rss;
+            else
+                return css;
+
+        }
+
+    }
+
+    public static int _findMaxCrossingArraySum (int low, int mid,  int high, int array[]) {
+        int leftSum = Integer.MIN_VALUE;
+        int rightSum = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int i = mid; i >= low ; i--) {
+            sum = sum + array[i];
+            if (sum > leftSum)
+                leftSum = sum;
+        }
+
+        sum = 0;
+        for (int i = mid + 1; i <= high ; i++) {
+            sum = sum + array[i];
+            if (sum > rightSum)
+                rightSum = sum;
+        }
+
+        // return max of either (left sum, right sum, left + right)
+        return Math.max(Math.max(leftSum, rightSum), leftSum+rightSum);
+    }
+
+
+
+}
