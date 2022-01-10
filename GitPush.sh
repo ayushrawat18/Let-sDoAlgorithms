f200370f9e13900d51d44826f888001bb27cf616
ayush.rawat <ayush.rawat@servicenow.com>
2022-01-10 18:42:59 +0530

    Fixed more problems

--- GitPush.sh
+++ GitPush.sh
@@ -1,7 +1,13 @@
+echo "Branch name : $1"; 
+echo "Checkin message : $2";
+
 git stash 
 git checkout main
+git pull --rebase
+echo "Creating branch : $1"
+git checkout -b "scratch/$1"
 git stash pop
-git checkout -b "scratch/dsalgo"
 git add *
-git commit -m "$1 "
+git commit -m "$2"
 git push 
+
