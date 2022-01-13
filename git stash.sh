6fe26ae59d14215df3e454be769eb00865526e7e
ayush.rawat <ayush.rawat@servicenow.com>
2022-01-13 20:23:51 +0530

    Solved Tree problems

--- git stash.sh
+++
@@ -1,7 +0,0 @@
-git stash 
-git checkout main
-git stash pop
-git checkout -b "scratch/dsalgo"
-git add *
-git commit -m "$1 "
-git push 
---
+++ src/Recursion/StringPartitionPalindrome.java
@@ -0,0 +1,58 @@
+package Recursion;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class StringPartitionPalindrome {
+
+    public static void main(String args[]) {
+
+        String str = "aabb";
+        Map<String, Integer> s = new HashMap<>();
+        s.isEmpty();
+
+
+        List<List<String>> ans = new ArrayList<>();
+        List<String> current = new ArrayList<>();
+
+        getPalindromicPartition(str, ans, current);
+
+        System.out.println(" "+ans.toString());
+    }
+
+    private static void getPalindromicPartition(String str, List<List<String>> ans, List<String> current ) {
+
+        if (str.length() == 0) {
+            ans.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            String partitionStr = str.substring(0, i+1);
+            String remStr = str.substring(i+1);
+
+            if (isPalindrome(partitionStr)) {
+                current.add(partitionStr);
+                getPalindromicPartition(remStr, ans, current);
+               // current.clear();
+                current.remove(current.size()-1);
+            }
+        }
+
+    }
+
+    private static boolean isPalindrome(String partitionStr) {
+        int i = 0;
+        int j = partitionStr.length()-1;
+
+        while (i<j) {
+            if (partitionStr.charAt(i) != partitionStr.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
