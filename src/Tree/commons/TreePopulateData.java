ca15e0c046ddd961771acf2ee9c99db554328d20
ayush.rawat <ayush.rawat@servicenow.com>
2022-02-06 22:19:52 +0530

    Find nodes at k distance, is heightBalanced, sumtree validation

--- src/Tree/commons/TreePopulateData.java
+++ src/Tree/commons/TreePopulateData.java
@@ -47,7 +47,7 @@ public class TreePopulateData {
         TreeNode node7 = new TreeNode(7,null, null );
         TreeNode node6 = new TreeNode(6,null, null );
         TreeNode node5 = new TreeNode(-2,null, null );
-        TreeNode node4 = new TreeNode(6, null, null);
+        TreeNode node4 = new TreeNode(8, null, null);
         TreeNode node2 = new TreeNode(2, node4, node5);
         TreeNode node3 = new TreeNode(13, node6, node7);
         TreeNode head = new TreeNode(30,node2, node3);
@@ -89,8 +89,8 @@ public class TreePopulateData {
            9    8
      */
     public TreeNode populateUnBalancedTreeData() {
-       // TreeNode node10 = new TreeNode(10, null, null);
-        TreeNode node9 = new TreeNode(9, null, null);
+        TreeNode node10 = new TreeNode(10, null, null);
+        TreeNode node9 = new TreeNode(9, node10, null);
         TreeNode node8 = new TreeNode(8, null, null);
         TreeNode node7 = new TreeNode(7,node8, null );
         TreeNode node6 = new TreeNode(6,null, null );
---
+++ src/Tree/practice/FindNodesAtKDist.java
@@ -0,0 +1,59 @@
+package Tree.practice;
+
+import Tree.commons.TreeNode;
+import Tree.commons.TreePopulateData;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindNodesAtKDist {
+
+
+    public static void main(String args[]) {
+
+        TreeNode root = new TreePopulateData().populateTreeData();
+        findElementsAtKDistance(root, 3, 0);
+    }
+
+    private static void findElementsAtKDistance(TreeNode root, int nodeV, int distance) {
+        List<Integer> ans = new ArrayList<>();
+      TreeNode node =  findNode(root, nodeV);
+
+        findNodesAtDistanceK(node,0, distance, ans);
+        System.out.println(ans);
+    }
+
+    /**
+     * we can also do this using BFS : track the levels add all the elements of the level distance
+     * return
+     * @param node
+     * @param currentDis
+     * @param distance
+     * @param ans
+     */
+    private static void findNodesAtDistanceK(TreeNode node, int currentDis, int distance, List<Integer> ans) {
+
+        if (node == null)
+            return;
+        if (currentDis == distance) {
+            ans.add(node.getData());
+            return;
+        }
+        findNodesAtDistanceK(node.getLeft(), currentDis+1, distance, ans);
+        findNodesAtDistanceK(node.getRight(), currentDis+1, distance, ans);
+    }
+
+    private static TreeNode findNode(TreeNode root, int nodeV) {
+
+        if (root == null)
+            return null;
+        if (root.getData() == nodeV)
+            return  root;
+        else {
+           TreeNode node =  findNode(root.getLeft(), nodeV);
+           if (node == null)
+               node =  findNode(root.getRight(), nodeV);
+           return  node;
+        }
+    }
+}
