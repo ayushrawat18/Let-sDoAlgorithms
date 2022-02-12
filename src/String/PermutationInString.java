f01adb1c5b00118a7be88deff096ac4760e85123
ayush.rawat <ayush.rawat@servicenow.com>
2022-02-12 12:50:34 +0530

    Solved sliding window problem

--- src/String/PermutationInString.java
+++ src/String/PermutationInString.java
@@ -6,22 +6,45 @@ public class PermutationInString {
 
 public static void main(String args[]) {
 
-    System.out.println(checkInclusion("ab", "eidboaoo"));
+    System.out.println(checkInclusion("adc", "dcda"));
 }
+
+    /**
+     * Sliding window problem
+     * @param s1
+     * @param s2
+     * @return
+     */
     public static boolean checkInclusion(String s1, String s2) {
 
-        Map<Character, Integer> count  = new HashMap<>();
-        for(int i=0; i<s2.length(); i++) {
-            count.put(s2.charAt(i), count.getOrDefault(s2.charAt(i), 0)+1);
+    int s1Arr[] = new int[128];
+    int s2Arr[] = new int[128];
+    int curWindowLen = 0;
+
+        for (int i=0; i<s1.length(); i++) {
+            s1Arr[s1.charAt(i)] += 1;
         }
 
-        for(int i=0; i<s1.length(); i++) {
-            Character s = s1.charAt(i);
-            if(count.containsKey(s) && count.get(s) > 0) {
-                count.put(s, count.get(s)-1);
-            } else {
-                return false;
+
+        for (int i = 0; i <s2.length() ; i++) {
+            s2Arr[s2.charAt(i)] +=1;
+            curWindowLen ++;
+
+            if (curWindowLen == s1.length()) {
+               if(validateArr(s1Arr, s2Arr))
+                    return true;
+
+               curWindowLen --;
+               s2Arr[s2.charAt(i-curWindowLen)] -=1;
             }
+        }
+        return false;
+    }
+
+    private static boolean validateArr(int[] s1Arr, int[] s2Arr) {
+        for (int i = 0; i < 128; i++) {
+            if (s1Arr[i] != s2Arr[i])
+                return false;
         }
         return true;
     }
