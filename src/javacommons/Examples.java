e6d3e18ef8e28ecdf5604c4f79c7ba39c7a034cd
ayush.rawat <ayush.rawat@servicenow.com>
2022-02-20 21:20:35 +0530

    Some basics and commonly used

--- src/javacommons/Examples.java
+++ src/javacommons/Examples.java
@@ -13,6 +13,19 @@ public class Examples {
         //Convert String to integer
         String ex1 = "0123";
         System.out.println(Integer.valueOf(ex1));
+
+        //intializing two diamensional array
+        int arr[][] = new int[10][2];
+        System.out.println(arr[0].length); // rows
+        System.out.println(arr.length); // columns
+
+        int arr2[] = new int[]{34, 454, 54}; // intialzing the elements of array
+
+        int arr3[] = {34, 454, 54}; // intialzing the elements of array
+
+
+        
+
         
     }
 }
