a25842b67cd80f4eac03eb96dad6ad6ea6a3d73c
ayush.rawat <ayush.rawat@servicenow.com>
2022-03-03 22:55:49 +0530

    Validate subsequence of strings

---
+++ src/String/MatchSubSeq.java
@@ -0,0 +1,23 @@
+package String;
+
+public class MatchSubSeq {
+
+    public static void main(String args[]) {
+        String s = "aaaaaa", t = "bbaaaa";
+        int idx = 0, matchChar =0;
+        for(int i =0; i< s.length(); i++) {
+
+            while (idx < t.length() && s.charAt(i) != t.charAt(idx)) {
+                idx++;
+            }
+
+            if(idx < t.length() && s.charAt(i) == t.charAt(idx)) {
+                matchChar++;
+                idx ++;
+            }
+        }
+
+
+        System.out.println(matchChar == s.length());
+    }
+}
